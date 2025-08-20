fd80ad4009e600a2ce56f1a16679b5429a5295c6
BryzoXu <xuerebbusiness@gmail.com>
2025-08-20 21:32:37 +1000

    Final Changes to Tax calculator

--- Week4/#Tax_Income.py
+++ Week4/#Tax_Income.py
@@ -5,9 +5,8 @@ income = float(input(f"Enter Taxable income: "))
 if income <= 20000:
     tax = income * 0.02
 elif income <= 50000:
-    tax = (income-20000) * 0.025 + 400
+    tax = (income-20000) * 0.025 + 400500
 else:
     tax = (income-50000) * 0.035 + 1150
 
-print(f"Tax Payable:{tax}")
-        
+print(f"Tax Payable: ${tax:.2f}")
