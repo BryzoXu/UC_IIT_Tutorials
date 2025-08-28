d1edb238ace3a349d7894c87718bb201d1c7c6ea
BryzoXu <xuerebbusiness@gmail.com>
2025-08-28 22:48:33 +1000

    Week 5 Case Study
    still WIP up to "Part E" all other functions working

--- Week 5/#Movies.py
+++ Week 5/#Movies.py
@@ -3,6 +3,10 @@
 #---
 #Global
 #---
+tickets_by_movie = {}
+revenue_by_movie = {}
+
+purchases = []  
 
 movies = {
     "Dune": 12.5,
@@ -11,32 +15,58 @@ movies = {
     "Spirited Away": 10.0
 }
 
-purchases = []  # list of (title, qty, price_each)
+
 
 #---
-#Function
+#Functions
 #---
 
-# Title, Tickets, Ticket Cost
-def total_cost(item):
+
+def total_cost(items):
     total = 0
     recipt = {}
+    discount = False  
+     
+    for x in items:
+        name = x[0]
+        qty = x[1]
+        price = x[2]
+        line_total = qty * price
+        if qty > 4:
+            line_total *= 0.9 # 10% discount
+            discount = True
+       
+        if name in recipt:  # already purchased this movie
+            old_qty, old_total = recipt[name]
+            recipt[name] = (old_qty + qty, old_total + line_total)
+        else:
+            recipt[name] = (qty, line_total)
+        
+        total += line_total
     
-    for i in range(len(item)):
-        x = item[i]
-        calc= x[1] * x[2]
-        rc = {item[0]: calc}
-        recipt.append(rc)
-        total += calc    
-    return total,recipt
+    return total, recipt, discount
 
 
 def available_movies():
     print("Available movies:")
     for title, price in movies.items():
         print(f"{title:14s} ${price:.2f}")
-    
 
+def analytics(tickets_by_movie, revenue_by_movie):
+    
+    
+    
+    return 
+
+def sale_summary(recipt):
+  
+    for item, (qty, _) in recipt.items(): 
+        tickets_by_movie[item] = tickets_by_movie.get(item, 0) + qty
+    
+    for item, (_, cost) in recipt.items():
+        revenue_by_movie[item] = revenue_by_movie.get(item, 0) + cost
+    
+     
 
 #---
 #MAIN
@@ -51,7 +81,7 @@ while True:
     if title.lower() == 'done':
         break
     if title in movies:
-        tickets = int(input(f"How many would you like tickets?: "))
+        tickets = int(input(f"How many tickets would you like?: "))
     else:
         print('Thats not avalible!')
         available_movies()
@@ -59,20 +89,35 @@ while True:
     q = (title, tickets, movies[title])
     purchases.append(q)
 
-total = total_cost(purchases)
+total, recipt, discount = total_cost(purchases)
+
+sale_summary(recipt)
+analytics(tickets_by_movie, revenue_by_movie)
+
 
 
 #---
-#Recipt
+#OUT
 #---
 
-print(total)
+mem_code = input(f"\nAre you a memeber?: ").lower()
+if "y" in mem_code:
+    total *= 0.95
+
+print("----------\n*Recipt* \n        ")
+
+for item, (qty, cost) in recipt.items():
+    print(f"{item:10s} {qty} @ ${movies[item]:.2f} = ${cost:.2f}")
+
+print(f"Your Total: ${total:.2f}")
+if discount == True:
+    print(f"**INCLUDES GROUP 10% DISCOUNT**")
 
 
-
-        
     
 
+        
+
     
         
     
@@ -82,10 +127,3 @@ print(total)
         
 
 
-
-# TODO: use a while loop that continues until the user enters 'done'
-# 1) ask for movie title (case sensitive is fine)
-# 2) if title not in movies, print the available keys and continue
-# 3) ask for quantity (int)
-# 4) append (title, qty, movies[title]) to purchases
-# 5) optional: track running subtotal
